b1685b9a09ff2dc394222835a8b9bcde53c970a7
PavelShturm85 <pavel.shturm@gmail.com>
2017-11-19 10:32:29 +0400

    переименвана функция и добавлен второй вариант ввода файла.

--- pprint_json.py
+++ pprint_json.py
@@ -2,7 +2,7 @@ import sys
 import json
 
 
-def load_data(enter_file):
+def load_json_data(enter_file):
     with open(enter_file) as json_file:
         json_content = json.load(json_file)
     return json_content
@@ -17,5 +17,9 @@ def pretty_print_json(uploaded_data):
 
 
 if __name__ == '__main__':
-    content = load_data(sys.argv[1])
+    if len(sys.argv) > 1:
+        input_file_name = sys.argv[1]
+    else:
+        input_file_name = input('Input filepath: ')
+    content = load_json_data(input_file_name)
     pretty_print_json(content)
